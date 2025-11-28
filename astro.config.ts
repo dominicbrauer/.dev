6f0c03f83b03f7b7d3371027ad586e4a10a85e66
dominicbrauer <dominicbrauer06@proton.me>
2025-11-28 09:13:27 +0100

    Add last untracked files

---
+++ astro.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'astro/config';
+import type { AstroIntegration } from 'astro';
+import node from '@astrojs/node';
+import db from '@astrojs/db';
+
+function devRoutes(): AstroIntegration {
+  return {
+    name: 'dev-routes',
+    hooks: {
+      'astro:config:setup': (params) => {
+        if (params.command === 'dev') {
+          params.injectRoute(
+          	{
+	            pattern: '/_test',
+	            entrypoint: './src/pages/_test/index.astro'
+          	}
+          )
+        }
+      }
+    }
+  }
+}
+
+export default defineConfig({
+  site: "https://dominicbrauer.dev",
+  output: "server",
+
+  devToolbar: {
+      enabled: false
+	},
+
+  adapter: node({
+      mode: 'standalone'
+	}),
+
+  image: {
+    domains: [
+      "shared.fastly.steamstatic.com/",
+      "cdn.akamai.steamstatic.com"
+    ]
+  },
+
+  integrations: [
+  	db(),
+   	devRoutes()
+  ]
+});
